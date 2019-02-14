a3185b0c12a6465a895efa74cbcdc948245fe013
Kaleb McKone <kaleb-mckone@uiowa.edu>
2019-02-13 22:41:09 -0600

    Remove uneeded code

--- server/controllers/assessments.controller.js
+++ server/controllers/assessments.controller.js
@@ -47,32 +47,6 @@ async function updateAssessmentData(reqData) {
     })
   })
 }
-//   // AssessmentModel.findOne(condition, (err, doc) => {
-//   //   if (err) console.log(err)
-//   //   if (reqData.assessments[0].data.recorded_data) {
-//   //     if (!fs.existsSync(path.join('assessmentRecordingData', reqData.user_id, reqData.assessments[0].assess_name))) {
-//   //       console.log('making directory')
-//   //       fs.mkdirSync(path.join('assessmentRecordingData', reqData.user_id, reqData.assessments[0].assess_name), {
-//   //         recursive: true
-//   //       }).catch((reason) => console.log(reason))
-// }
-// const fileName = 'assessmentRecordingData/' + reqData.user_id + '/' + reqData.assessments[0].assess_name + '_' + reqData.assessments[0].data.recorded_data[0].prompt_number
-// fs.writeFile(fileName, JSON.stringify(reqData.assessments[0].data.recorded_data[0]), (err) => {
-//   if (err) {
-//     console.log(err)
-//   }
-// })
-// const dataWithPath = {
-//   prompt_number: reqData.assessments[0].data.recorded_data[0].prompt_number,
-//   recorded_file_path: fileName
-// }
-// reqData.assessments[0].data = dataWithPath
-// }
-// doc.assessments.push(reqData.assessments[0])
-// doc.google_speech_to_text_assess.push(reqData.google_speech_to_text_assess[0])
-// doc.save()
-// })
-// }
 
 async function pushOneAudioAssessmentData(reqData) {
   const userID = reqData.user_id
@@ -96,24 +70,6 @@ async function pushOneAudioAssessmentData(reqData) {
       }
     })
   })
-  // AssessmentModel.findOne(condition, (err, doc) => {
-  //   if (err) console.log(err)
-  //   for (let i = 0; i < doc.assessments.length; i++) {
-  //     if (doc.assessments[i].assess_name === reqData.assessments[0].assess_name) {
-  //       // doc.assessments[i].data.recorded_data.push(reqData.assessments[0].data.recorded_data[0])
-  //       doc.assessments[i].data.recorded_data.push({
-  //         prompt_number: reqData.assessments[0].data.recorded_data[0].prompt_number,
-  //         recorded_file_path: fileName
-  //       })
-  //       if (reqData.assessments[0].completed === true) {
-  //         doc.assessments[i].completed = true
-  //       }
-  //       doc.markModified('assessments')
-  //       break
-  //     }
-  //   }
-  //   doc.save()
-  // })
 }
 
 function getUserAssessmentData(searchUserId) {
@@ -177,7 +133,6 @@ function getNextUserID() {
         resolve(currentID)
       }
     })
-    // return UserIDCounterModel.findOne({})
   })
 }
 
