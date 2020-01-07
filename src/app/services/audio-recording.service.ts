df987d30ac34e87119e3c581cb9dea6ac7777da9
Ben Tysseling <benjamin-tysseling@uiowa.edu>
2020-01-06 18:00:52 -0600

    adding stereo setting to options

--- src/app/services/audio-recording.service.ts
+++ src/app/services/audio-recording.service.ts
@@ -128,7 +128,11 @@ export class AudioRecordingService {
   private record(): void {
     const config = {
       type: 'audio',
-      mimeType: 'audio/webm'
+      mimeType: 'audio/webm',
+      recorderType: 'StereoAudioRecorder',
+      sampleRate: "44100",
+      bufferSize: "4096",
+      numberOfAudioChannels: "2"
     };
     this.setCurrentlyRecording(true);
     this.recorder = new RecordRTC.StereoAudioRecorder(this.stream, config);
